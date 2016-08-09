78a2901b97d8d8dddf5cc03456fb36e386f0b8f7
Mason <bigger831@sina.com>
2016-08-09 16:19:48 +0800

    完善热处理编辑卡片，提交到dev

--- edit/edit_heat.php
+++ edit/edit_heat.php
@@ -6,7 +6,7 @@
  * Time: 21:51
  */
 ?>
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -17,18 +17,53 @@
     <script>
         $(function () {
 
+            var panelWidth = $(".panel-content").width();
+            $(".part-property").width(panelWidth - 50);
+
+            // 初始化中间部分
             var
-                contentWidth = $(".part-property").width(),
+                contentWidth = panelWidth - 50,
                 contentHeight = $(".heat-craft table").height();
-            $(".content").width(contentWidth).height(contentHeight);
 
-//            console.log(tableHeight);
-            $("#image").height(contentHeight - 1);
 
+            $(".content").width(contentWidth - 70).height(contentHeight);   //根据上面表格设置中间部分宽，根据下面表格设置高
+
+//            console.log(contentWidth);
+            $("#image").height(contentHeight - 1);  //设置和右边表格一样高
+
+
+            // 实现图片预览
+            var
+                fileInput = document.getElementById("chooseDwg"),
+                preview = document.getElementById("image");
+
+            fileInput.addEventListener('change', function () {
+                preview.style.backgroundImage = '';
+                if (!fileInput.value) {
+                    return;
+                }
+
+
+                var imageFile = fileInput.files[0];
+                if (imageFile.type !== 'image/jpeg' && imageFile.type !== 'image/png' && imageFile.type !== 'image/gif') {
+                    alert('不是有效的图片文件!');
+                    return;
+                }
+
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    var data = e.target.result;
+                    preview.style.backgroundImage = 'url(' + data + ')';
+                };
+                reader.readAsDataURL(imageFile);
+            });
         });
+        function choose() {
+            $("#chooseDwg").click();
+        }
+
     </script>
     <style type="text/css">
-
         table {
             border-collapse: separate;
             border-spacing: 10px 15px;
@@ -39,26 +74,37 @@
             height: 25px;
         }
 
+        table caption {
+            font-family: Arial;
+            font-size: 30px;
+        }
+
+        .content {
+            padding-left: 30px;
+            padding-right: 30px;
+        }
+
         .dwg {
             width: 40%;
             height: auto;
-            background-color: #00FFFF;
+            /*background-color: #00FFFF;*/
             margin: 5px auto;
             float: left;
             text-align: center;
         }
 
         #image {
-            width: 640px;
+            width: auto;
             border: 1px solid #ccc;
             background-size: contain;
-            background: no-repeat center center;
+            background-repeat: no-repeat;
+            background-position: center center;
         }
 
         .heat-craft {
             width: 60%;
             height: auto;
-            background-color: #5466da;
+            /*background-color: #5466da;*/
             margin: 5px auto;
             float: right;
 
@@ -67,12 +113,12 @@
     </style>
 </head>
 <body>
-<div class="edit-panel" style="width: 90%;">
+<div class="edit-panel" style="width:90%;">
     <div class="panel-head">
         <p>添加锻造工艺</p>
     </div>
     <div class="panel-content">
-        <form>
+        <form autocomplete="on">
             <div class="part-property">
                 <table align="center">
                     <tr>
@@ -105,7 +151,7 @@
                 </div>
                 <div class="heat-craft">
                     <table align="center">
-                        <caption align="left">固溶处理</caption>
+                        <caption>固溶处理</caption>
                         <tr>
                             <th>装炉温度（℃）</th>
                             <td><input type="text"></td>
@@ -136,7 +182,8 @@
             </div>
 
             <div class="edit-button">
-                <input class="ui-button ui-widget ui-corner-all" type="button" value="选择图片">
+                <input id="chooseDwg" type="file" style="" required value="选择图纸">
+                <button class="ui-button ui-widget ui-corner-all" onclick="choose()" style="visibility: hidden">选择图纸</button>
                 <input class="ui-button ui-widget ui-corner-all" type="submit" value="提交">
                 <input class="ui-button ui-widget ui-corner-all" type="reset" value="重置">
             </div>
